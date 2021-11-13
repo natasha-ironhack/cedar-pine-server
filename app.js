e47730323abf039d4aad321c990de751426a0843
Natasha Smith <pisceanchic@gmail.com>
2021-11-13 15:01:49 +0100

    added frontend to server

--- app.js
+++ app.js
@@ -14,6 +14,9 @@ const app = express();
 // ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
 require("./config")(app);
 
+const path = require("path");
+app.use(express.static(path.join(__dirname, "public")));
+
 // 👇 Start handling routes here
 // Contrary to the views version, all routes are controlled from the routes/index.js
 const allRoutes = require("./routes");
@@ -23,9 +26,20 @@ app.use("/api", allRoutes);
 const index = require("./routes/index"); // <== already included
 app.use("/", index); // <== already included
 
+//(backend route)
 const candleImageRouter = require("./routes/candles.routes"); // <== has to be added
 app.use("/api", candleImageRouter); // <== has to be added
 
+//doesn't start with /api? will go here next:
+
+//(front end route)
+//will check for front end routes that exist in a react application
+app.use((req, res, next) => {
+  // If no routes match, send them the React HTML.
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+//(neither front end route or back end route? will go here:)
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 
